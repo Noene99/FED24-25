36d0025105dff545e46ccb99deb4304ebb8d3477
Noene99 <sandra-strijker@hotmail.com>
2024-09-26 21:30:15 +0200

    hamburgermenu met js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,41 @@
-// JavaScript Document
-console.log("hi");
+// // JavaScript Document
+// console.log("hi");
 
 
-// Navigatie Button
-const button = document.querySelector("button");
-const header = document.querySelector("header");
+// // Navigatie Button
+// const button = document.querySelector("button");
+// const header = document.querySelector("header");
 
-button.onclick = toggleMenu;
+// button.onclick = toggleMenu;
 
-function toggleMenu (){
-  header.classList.toggle("toonMenu")
-}
-
-
+// function toggleMenu (){
+//   header.classList.toggle("toonMenu")
+// }
 
 
-/* MENU */
 
-// stap 1: zoek de menu-button op en sla die op in een variabele
-let hamburgerMenu = document.querySelector("header button");
-let deUl = document.querySelector("header nav");
 
-// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
-hamburgerMenu.onclick = toggleMenu;
+// /* MENU */
 
-// stap 3: voeg in de functie een class toe aan de nav
+// // stap 1: zoek de menu-button op en sla die op in een variabele
+// let hamburgerMenu = document.querySelector("header button");
+// let deUl = document.querySelector("header nav");
+
+// // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+// hamburgerMenu.onclick = toggleMenu;
+
+// // stap 3: voeg in de functie een class toe aan de nav
+// function toggleMenu() {
+//   deUl.classList.toggle("toonMenu");
+// }
+
+
+// JavaScript voor het toggelen van het menu
+const menuButton = document.querySelector('button');
+const header = document.querySelector('header'); // Kies de header
+
+menuButton.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
-  deUl.classList.toggle("toonMenu");
+  header.classList.toggle('toonMenu'); // Toggle de klasse op de header
 }
