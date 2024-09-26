49dda42a1b6316c5225b406b4dfa979e97a60e7e
Noene99 <sandra-strijker@hotmail.com>
2024-09-26 16:57:10 +0200

    Header

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,20 @@ button.onclick = toggleMenu;
 function toggleMenu (){
   header.classList.toggle("toonMenu")
 }
+
+
+
+
+/* MENU */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+let hamburgerMenu = document.querySelector("header button");
+let deUl = document.querySelector("header nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+hamburgerMenu.onclick = toggleMenu;
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu() {
+  deUl.classList.toggle("toonMenu");
+}
